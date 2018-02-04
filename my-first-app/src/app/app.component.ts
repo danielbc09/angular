fd66bf69cd88004f61f2a854a360a591b2301e7f
Your Name <you@example.com>
2018-02-04 15:15:08 -0500

    Solution

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { log } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  paragraphContent = `I like angular!!`;
+  show = true;
+  logs = [];
+
+  toggleParagraph() {
+    this.show = !this.show;
+    this.logs.push(this.logs.length + 1);
+  }
+
+  getColor(item) {
+    return item >= 5 ? 'blue' : '';
+  }
 }
