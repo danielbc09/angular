fa2bd3d16d0490e878b4ddc295802fcabaa50fd1
Your Name <you@example.com>
2018-02-13 22:56:48 -0500

    Ingredients list

--- basic-project/src/app/shared/ingredients.model.ts
+++ basic-project/src/app/shared/ingredients.model.ts
@@ -2,7 +2,7 @@ export class Ingredient {
     public name: string;
     public amount: number;
 
-    constructorname(name: string, amount: number) {
+    constructor(name: string, amount: number) {
         this.name = name;
         this.amount = amount;
     }
--- basic-project/src/app/shopping-list/shopping-list.component.ts
+++ basic-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 6),
+    new Ingredient('Tomatoes', 20),
+  ];
+
   constructor() { }
 
   ngOnInit() {
