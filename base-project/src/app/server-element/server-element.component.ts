5495b55910e48b05fe57d8adcb64b8a48e61b8aa
Your Name <you@example.com>
2018-02-14 23:07:02 -0500

    Binding custom Components

--- base-project/src/app/server-element/server-element.component.ts
+++ base-project/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,26 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  newServerName = '';
-  newServerContent = '';
-  constructor() { }
+  @Input ()
+  element: {type: string, name: string, content: string};
+
+  constructor() {}
 
   ngOnInit() {
   }
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 }
