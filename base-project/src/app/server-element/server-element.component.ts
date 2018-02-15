47f31d41be1dbbc4beb5ad5c3e39bdfe7a41b948
Your Name <you@example.com>
2018-02-14 22:49:50 -0500

    creating components

---
+++ base-project/src/app/server-element/server-element.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
