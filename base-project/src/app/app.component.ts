a8f53866b4cb2849e64bcc442fc70b0b8a1e5d28
Your Name <you@example.com>
2018-02-15 00:05:49 -0500

    Binding CUstom Event

--- base-project/src/app/app.component.ts
+++ base-project/src/app/app.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+    console.log('onServerAdded' + this.serverElements);
+  }
+
+  onBlueprintAdded(bluePrint: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrint.serverName,
+      content: bluePrint.serverContent,
+    });
+    console.log('onBlueprintAdded' + this.serverElements);
+  }
 
 }
