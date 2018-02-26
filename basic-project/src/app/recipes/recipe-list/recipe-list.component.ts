56654272d5c9dbc0f27407e31fbd17b43e052f86
Your Name <you@example.com>
2018-02-26 16:02:56 -0500

    Passing recipe Data (Parent to Child)

--- basic-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basic-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'Simple Test', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('A test Recipe', 'Simple Test', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('A test Recipe', 'Simple Test2', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('A test Recipe', 'Simple Test3', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
   ];
 
   constructor() { }
