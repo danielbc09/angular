8ede19ee14ee9753ad322eab8030f3953caa5cf5
Your Name <you@example.com>
2018-02-26 20:11:47 -0500

    Using HostBinding to chenge the attributes

--- base-project/src/app/better-highlight/better-highlight.directive.ts
+++ base-project/src/app/better-highlight/better-highlight.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
@@ -13,10 +14,14 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseOver (eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
+
+
 }
