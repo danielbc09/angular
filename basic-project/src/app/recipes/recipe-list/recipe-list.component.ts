212131b3565dad6b1167c61abdc563bc2318480b
Your Name <you@example.com>
2018-02-28 00:44:47 -0500

    Managing Recipes in a Recipe Service

--- basic-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basic-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'Simple Test',  'http://lorempixel.com/200/200/food/'),
-    new Recipe('Another Recipe', 'Simple Test2', 'http://lorempixel.com/200/200/food/'),
-    new Recipe('Another Dummy Recipe', 'Simple Test3', 'http://lorempixel.com/200/200/food/'),
-  ];
+  
   @Output()
   recipeWasSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  recipes: Recipe[];
+
+
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- basic-project/src/app/recipes/recipe.service.ts
+++ basic-project/src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
-export class RecipeService {
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('A test Recipe', 'Simple Test',  'http://lorempixel.com/200/200/food/'),
+        new Recipe('Another Recipe', 'Simple Test2', 'http://lorempixel.com/200/200/food/'),
+        new Recipe('Another Dummy Recipe', 'Simple Test3', 'http://lorempixel.com/200/200/food/'),
+      ];
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
 }
