c5007c774139cbb3cba9473012af3181612157e7
Your Name <you@example.com>
2018-02-28 02:06:36 -0500

    Adding Ingredients to recipes

--- basic-project/src/app/recipes/recipe.service.ts
+++ basic-project/src/app/recipes/recipe.service.ts
@@ -1,14 +1,41 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test Recipe', 'Simple Test',  'http://lorempixel.com/200/200/food/'),
-        new Recipe('Another Recipe', 'Simple Test2', 'http://lorempixel.com/200/200/food/'),
-        new Recipe('Another Dummy Recipe', 'Simple Test3', 'http://lorempixel.com/200/200/food/'),
+        new Recipe(
+            'A test Recipe',
+            'Simple Test',
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Combo Burger',
+            'Simple Test2',
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+            [
+                new Ingredient('Buns', 1),
+                new Ingredient('Meat', 1),
+                new Ingredient('Tomatoes', 1),
+                new Ingredient('Lettuce', 20)
+            ]),
+        new Recipe(
+            'Soup',
+            'Simple Test3',
+            'https://upload.wikimedia.org/wikipedia/commons/7/77/Beef_stew.jpg',
+            [
+                new Ingredient('Water', 1),
+                new Ingredient('Carrots', 2),
+                new Ingredient('Beans', 1),
+                new Ingredient('Sausages', 3)
+            ]
+            ),
       ];
 
       getRecipes() {
