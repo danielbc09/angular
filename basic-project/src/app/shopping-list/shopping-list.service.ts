5ecca0b59a7837df8237551e5db5cb56671aae8d
Your Name <you@example.com>
2018-02-28 01:30:35 -0500

    Adding Shopping list Service to add Ingredient

--- basic-project/src/app/shopping-list/shopping-list.service.ts
+++ basic-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,17 @@
+import { Ingredient } from '../shared/ingredients.model';
+
 export class ShoppingListService {
 
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 6),
+        new Ingredient('Tomatoes', 20),
+      ];
+
+     getIngredients() {
+        return this.ingredients.slice();
+     }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+    }
 }
