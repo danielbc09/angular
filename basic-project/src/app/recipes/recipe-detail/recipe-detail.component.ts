2eebd8c15707134fc05c3403cace8d0124ff560c
Your Name <you@example.com>
2018-03-03 17:43:22 -0500

    Configure router parameters in recipe-detail-compoent and for get the recipe

--- basic-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ basic-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { Ingredient } from '../../shared/ingredients.model';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,12 +12,21 @@ import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input()
   recipe: Recipe;
+  id: number;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService,
+              private recipeServices: RecipeService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+        .subscribe(
+          (params: Params) => {
+            this.id = +params['id'];
+            this.recipe = this.recipeServices.getRecipe(this.id);
+          }
+        );
   }
 
   addRecipeIngredients(recipeIngredients: Ingredient[]) {
--- basic-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ basic-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, EventEmitter} from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 import { ShoppingListService } from '../../../shopping-list/shopping-list.service';
 
 
@@ -14,15 +13,9 @@ export class RecipeItemComponent implements OnInit {
   @Input()
   recipe: Recipe;
 
-
-  constructor(private recipeService: RecipeService, private shoppingListService: ShoppingListService) { }
-
   ngOnInit() {
   }
 
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 
 
 }
