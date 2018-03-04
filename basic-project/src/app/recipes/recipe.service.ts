202e37e79082694130e94899fee15960361a25b3
Your Name <you@example.com>
2018-03-03 21:51:28 -0500

    Refactoring App, Changing emmit to subject event

--- basic-project/src/app/recipes/recipe.service.ts
+++ basic-project/src/app/recipes/recipe.service.ts
@@ -1,11 +1,8 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredients.model';
 
 
 export class RecipeService {
-
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
--- basic-project/src/app/shopping-list/shopping-list.service.ts
+++ basic-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,11 @@
 import { Ingredient } from '../shared/ingredients.model';
 import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 
 export class ShoppingListService {
 
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 6),
@@ -17,12 +18,12 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]) {
        /* spread operator ES6*/
          this.ingredients.push(... ingredients);
-       this.ingredientsChanged.emit(this.ingredients.slice());
+       this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
