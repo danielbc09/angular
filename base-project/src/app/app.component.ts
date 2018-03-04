1353e5d74c42cfd9f3c77c7c8f56942e350fb689
Your Name <you@example.com>
2018-03-03 21:18:24 -0500

    Using Subject for cross component communication

--- base-project/src/app/app.component.ts
+++ base-project/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private userService: UserService){}
+  user1Activated = false;
+  user2Activated = false;
+  ngOnInit(): void {
+    this.userService.userActivated.subscribe(
+      (id: number) => {
+        if ( id === 1) {
+          this.user1Activated = true;
+        } else if (id === 2) {
+          this.user2Activated = true;
+        }
+      }
+    );
+  }
 }
