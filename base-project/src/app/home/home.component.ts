3cd3a8ee08935e46566318e7e7074470320b02be
Your Name <you@example.com>
2018-03-03 20:03:02 -0500

    Custom Observables

--- base-project/src/app/home/home.component.ts
+++ base-project/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+// tslint:disable-next-line:import-blacklist
 import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,11 +13,29 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
-      }
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package ');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package ');
+      }, 4000);
+      setTimeout(() => {
+        observer.error('this does not work');
+      }, 5000);
+    });
+
+    myObservable.subscribe(
+      (data: string) => {console.log(data); },
+      (error: string) => {console.log(error); },
+      () => {console.log('complete'); }
     );
   }
 
