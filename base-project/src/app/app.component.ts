bda0b3ab79329ceef4949e71111e7e42c2b555eb
Daniel <jdanielbc193@gmail.com>
2018-03-15 14:49:38 -0500

    Submitting the Form

--- base-project/src/app/app.component.ts
+++ base-project/src/app/app.component.ts
@@ -15,7 +15,11 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'username': new FormControl(null),
       'email': new FormControl(null),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
     });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm); 
   }
 }
