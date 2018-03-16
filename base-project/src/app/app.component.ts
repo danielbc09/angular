c6de68627f8b86488371ecb1f7f57f75cc926e59
Daniel <jdanielbc193@gmail.com>
2018-03-16 16:55:22 -0500

    Init assigment

--- base-project/src/app/app.component.ts
+++ base-project/src/app/app.component.ts
@@ -9,54 +9,17 @@ import { reject } from 'q';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  
+  singupForm: FormGroup;
 
-  genders = ['male', 'female'];
-  signupForm: FormGroup;
-  forbiddenUserNames = ['Dany', 'Anna'];
-
-  ngOnInit() {
-    this.signupForm = new FormGroup({
-      'userData': new FormGroup({ 
-        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)) ,
-      }),  
-      'gender': new FormControl('male'),
-      'hobbies': new FormArray([]),
+  ngOnInit(): void {
+    this.singupForm = new FormGroup({
+      'projectname': new FormControl('Project', Validators.required),
+      'email': new FormControl('test@email.com', [Validators.required, Validators.email]), 
     });
-    // this.signupForm.valueChanges.subscribe(
-    //   (value) => console.log(value)
-    // );
-    this.signupForm.statusChanges.subscribe(
-      (value) => console.log(value)
-    );
   }
 
   onSubmit() {
-    console.log(this.signupForm); 
-  }
-
-  onAddHobby() {
-    const control = new FormControl(null, Validators.required);
-    (<FormArray>this.signupForm.get('hobbies')).push(control);
-  }
-
-  forbiddenNames (control: FormControl):{[s: string]: boolean} {
-    if(this.forbiddenUserNames.indexOf(control.value) !== -1) {
-      return{'nameIsForbidden': true}
-    }
-    return null;
-  }
-
-  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
-    const promise = new Promise<any> ((resolve, reject) => {
-      setTimeout(() => {
-        if( control.value === 'test@test.com') {
-          resolve({ 'emailIsForbidden': true});
-        } else {
-          resolve(null);
-        }
-      }, 1500);
-    });
-    return promise;
+    console.log(this.singupForm);
   }
 }
