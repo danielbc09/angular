abeefa7f2a6d6dc9341110fdb277743b44c4a96a
Daniel <jdanielbc193@gmail.com>
2018-03-21 17:20:16 -0500

    Deleting Ingredients

--- basic-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ basic-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -98,4 +98,7 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
 } 
