b22bdc1b05afb2d532ea09f3ccaef9b516335f57
Daniel <jdanielbc193@gmail.com>
2018-03-23 15:36:04 -0500

    Async Pipes

--- base-project/src/app/app.component.ts
+++ base-project/src/app/app.component.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  appStatus = new Promise((resolve, reject) => 
+  {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
   servers = [
     {
       instanceType: 'medium',
