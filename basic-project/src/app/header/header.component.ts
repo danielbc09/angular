31566242076f87135476ff1bdf8634099610482a
Daniel <jdanielbc193@gmail.com>
2018-04-30 17:28:55 -0500

    Implemmenting get

--- basic-project/src/app/header/header.component.ts
+++ basic-project/src/app/header/header.component.ts
@@ -22,4 +22,8 @@ export class HeaderComponent {
                 }
             );
     }
+
+    fetchRecipes() {
+        this.dataStorageService.getRecipes();
+    }
 }
--- basic-project/src/app/recipes/recipe.service.ts
+++ basic-project/src/app/recipes/recipe.service.ts
@@ -62,4 +62,10 @@ export class RecipeService {
         this.recipesChanged.next(this.recipes.slice());
 
       }
+
+      setRecipes(recipes: Recipe[]) {
+            this.recipes = recipes;
+            this.recipesChanged.next(this.recipes.slice());
+      }
+
 }
