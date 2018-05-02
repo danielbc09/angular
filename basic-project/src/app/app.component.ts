a35c2e21796b0f561326ea4de0f2591ebc81a613
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-05-02 16:16:05 -0500

    Setting up firebase SDK

--- basic-project/src/app/app.component.ts
+++ basic-project/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'app';
   loadedFeature = 'recipe';
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDxxvp0wnguCpMdhxJG3cOYTe52CPcbmHo",
+      authDomain: "ng-recipe-book-985da.firebaseapp.com",
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
