f6a1d42a529682a3f9475c43be92a5d2b7f5733b
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-05-03 17:49:16 -0500

    Creating Basic Core Module

--- basic-project/src/app/app.module.ts
+++ basic-project/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HeaderComponent} from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -12,13 +11,11 @@ import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreMoudule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,6 +24,7 @@ import { HomeComponent } from './home/home.component';
     SharedModule,
     ShoppingListModule,
     AuthModule,
+    CoreMoudule,
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
