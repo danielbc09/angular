edc2b0dd3b72e16704a03c13d86c7c23057f5e04
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-05-03 18:02:44 -0500

    Restructuring Services uses child Injector

--- basic-project/src/app/app.module.ts
+++ basic-project/src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
 import { HttpModule } from '@angular/http';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -26,7 +21,6 @@ import { CoreMoudule } from './core/core.module';
     AuthModule,
     CoreMoudule,
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
